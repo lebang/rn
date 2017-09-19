98fa80264c78cf3856b4198e27f301ac17c63782
libang <libang@xiaomi.com>
2017-09-19 11:00:06 +0800

    update

--- index.android.js
+++ index.android.js
@@ -2,9 +2,9 @@ import { AppRegistry } from 'react-native'
 import App from './js/App'
 import List from './js/List'
 import Register from './js/Register'
-import Nav from './js/Nav'
+//import Nav from './js/Nav'
 
 AppRegistry.registerComponent('mainApp', () => App)
 AppRegistry.registerComponent('flatList', () => List)
-AppRegistry.registerComponent('register', () => Nav)
+//AppRegistry.registerComponent('register', () => Nav)
 //AppRegistry.registerComponent('register', () => Register)
